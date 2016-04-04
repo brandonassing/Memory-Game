7dcb129d5d946eb1343d1f7ac1aacf2570bcc29d
brandonassing <bassing@uwo.ca>
2016-04-04 14:59:24 -0400

    First Commit

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    /// <summary>
+    /// Controls game over screen
+    /// </summary>
+
+    public Text resultGT, scoreGT, timeGT;
+    public AudioSource win, lose;
+
+    void Start()
+    {
+        //Changes text based on game results
+
+        resultGT = GameObject.Find("Result").GetComponent<Text>();
+        scoreGT = GameObject.Find("Score").GetComponent<Text>();
+        timeGT = GameObject.Find("Time").GetComponent<Text>();
+
+        if (Main.won)
+        {
+            resultGT.text = "YOU    /    WIN";
+            win.Play();
+        }
+        else
+        {
+            resultGT.text = "YOU    /   LOSE";
+            lose.Play();
+        }
+        scoreGT.text = "SCORE /  " + Main.finalscore;
+        timeGT.text = "TIME   /  " + Main.finalMinutes + ":" + Main.finalSeconds;
+    }
+
+}
---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ScreenManager : MonoBehaviour {
+
+    /// <summary>
+    /// Controls scene changes
+    /// </summary>
+    /// 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    public void LoadPlay()
+    {
+        SceneManager.LoadScene("_Scene_0");
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
